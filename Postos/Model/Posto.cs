d5b0d0653a7920a445145d2a6843a93d8e90f65e
Denis Viana <denisvcosta@gmail.com>
2019-05-06 20:30:35 -0300

    Some changes

---
+++ Postos/Model/Posto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Postos.Model
+{
+    public class Posto
+    {
+        String Uid { get; set; }
+        String Estado { get; set; }
+        String Municipio { get; set; }
+        String Nome { get; set; }
+        String Bandeira { get; set; }
+        double ValorVenda { get; set; }
+        double ValorCompra { get; set; }
+    }
+}
--- Postos/Program.cs
+++ Postos/Program.cs
@@ -12,10 +12,20 @@ namespace Postos
 {
     public class Program
     {
+
+        const String URL = "http://www.anp.gov.br/arquivos/dadosabertos/precos/precos-semanais_ultimas-4-semanas_gasolina-etanol.csv";
+
         public static void Main(string[] args)
         {
+
+            var wc = new System.Net.WebClient();
+            Console.WriteLine("Download file...");
+            wc.DownloadFile(URL, @"c:\temp\myfile.csv");
+
             CreateWebHostBuilder(args).Build().Run();
+                  
         }
+
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
